b997adb3876c6ec37713ac88d31efea483bce84c
Max Kim <maxjukim@gmail.com>
2020-07-12 00:35:49 -0700

    Refactored a bit and WIP of cursor.

---
+++ Floodgate/Assets/Scripts/Player/TopDownController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+
+
+    public GameObject targetIndicatorPrefab;
+    GameObject targetObject;
+
+    //Mouse cursor Camera offset effect
+    Vector2 playerPosOnScreen;
+    Vector2 cursorPosition;
+    Vector2 offsetVector;
+    //Plane that represents imaginary floor that will be used to calculate Aim target position
+    Plane surfacePlane = new Plane();
+
+    private void Awake()
+    {
+        if (targetIndicatorPrefab)
+        {
+            targetObject = Instantiate(targetIndicatorPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
